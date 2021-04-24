094ac1bcb3e0867ec79f009c47cd740eda69158e
MariaAzrar <mariaazrar2000@gmail.com>
2021-04-25 00:58:25 +0500

    Dropdown worked:

--- feedbackapp/lib/main.dart
+++ feedbackapp/lib/main.dart
@@ -68,16 +68,92 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Feeback(context),
+      body: Feedback(),
+    );
+  }
+}
+
+class Feedback extends StatefulWidget {
+  Feedback({Key? key}) : super(key: key);
+  @override
+  _FeedbackState createState() => _FeedbackState();
+}
+
+class _FeedbackState extends State<Feedback> {
+  String dropdownvalue = 'Newest First';
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+            child: DropdownButton(
+          value: dropdownvalue,
+          icon: Icon(
+            Icons.arrow_drop_down,
+            size: 0.0,
+            color: Colors.white,
+            ),
+          underline: Container(height: 0.0),
+          onChanged: (String? newV) {
+            setState(() {
+              dropdownvalue = newV!;
+            });
+          },
+          items: <String>[
+            'Newest First',
+            'Oldest First',
+            'Value High-Low',
+            'Value Low-High'
+          ].map((String value) {
+            return DropdownMenuItem(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        )),
+      ],
     );
   }
 }
 
 
-Widget Feeback(BuildContext context) {
-  return Row(
-    children: [
-      Text("I am here")
-    ],
-  );
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String dropdownValue = 'One';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: <String>['One', 'Two', 'Free', 'Four']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
 }
