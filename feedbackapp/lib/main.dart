aa6bd3917e5dd8620b11dfb28bdb25dc9aa6bfa0
MariaAzrar <mariaazrar2000@gmail.com>
2021-04-25 01:58:25 +0500

    Trying to use row to overlap 2 widgets

--- feedbackapp/lib/main.dart
+++ feedbackapp/lib/main.dart
@@ -89,17 +89,16 @@ class _FeedbackState extends State<Feedback> {
       //Column for entire feedback form
       crossAxisAlignment: CrossAxisAlignment.start, //Align elements to left
       children: [
-        Container(  //Container for dropdown
+        Container(
+            //Container for dropdown
             padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
             child: DropdownButtonFormField(
               decoration: InputDecoration(
-                labelText: 'Sort By',
-                labelStyle: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 21
-                )
-              ),
+                  labelText: 'Sort By',
+                  labelStyle: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 19)),
               value: dropdownvalue,
               icon: Icon(
                 Icons.arrow_drop_down,
@@ -126,9 +125,51 @@ class _FeedbackState extends State<Feedback> {
                 );
               }).toList(),
             )),
-        Container(  //Conatiner for transaction types
-
-        ),
+        Container(
+            //Conatiner for transaction types
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, //Align elements to left
+              children: [
+                Text(
+                  "Transaction Types",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                ),
+                Container(
+                  //button container
+                  child: Row(
+                    mainAxisAlignment:
+                        MainAxisAlignment.center, //Align elements to left
+                    children: [
+                      RaisedButton(
+                        padding: EdgeInsets.fromLTRB(12, 21, 12, 21),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          side: BorderSide(color: Colors.black),
+                        ),
+                        onPressed: () {},
+                        color: Colors.white,
+                        textColor: Colors.black,
+                        child: Text("Money In", style: TextStyle(fontSize: 15)),
+                      ),
+                      RaisedButton(
+                        padding: EdgeInsets.fromLTRB(12, 21, 12, 21),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          side: BorderSide(color: Colors.black),
+                        ),
+                        onPressed: () {},
+                        color: Colors.white,
+                        textColor: Colors.black,
+                        child:
+                            Text("Money Out", style: TextStyle(fontSize: 15)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )),
       ],
     );
   }
