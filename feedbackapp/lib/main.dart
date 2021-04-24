39a0a7c817a5a2cb51fa0f793464f44b43651bd6
MariaAzrar <mariaazrar2000@gmail.com>
2021-04-25 01:34:54 +0500

    Dropdown Completed

--- feedbackapp/lib/main.dart
+++ feedbackapp/lib/main.dart
@@ -29,6 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Row(
           //for the cross icon we need to use row
@@ -84,76 +85,51 @@ class _FeedbackState extends State<Feedback> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
+      //Column for entire feedback form
+      crossAxisAlignment: CrossAxisAlignment.start, //Align elements to left
       children: [
-        Container(
-            child: DropdownButton(
-          value: dropdownvalue,
-          icon: Icon(
-            Icons.arrow_drop_down,
-            size: 0.0,
-            color: Colors.white,
-            ),
-          underline: Container(height: 0.0),
-          onChanged: (String? newV) {
-            setState(() {
-              dropdownvalue = newV!;
-            });
-          },
-          items: <String>[
-            'Newest First',
-            'Oldest First',
-            'Value High-Low',
-            'Value Low-High'
-          ].map((String value) {
-            return DropdownMenuItem(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
-        )),
+        Container(  //Container for dropdown
+            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+            child: DropdownButtonFormField(
+              decoration: InputDecoration(
+                labelText: 'Sort By',
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 21
+                )
+              ),
+              value: dropdownvalue,
+              icon: Icon(
+                Icons.arrow_drop_down,
+                size: 0.0,
+                color: Colors.white,
+              ),
+              onChanged: (String? newV) {
+                setState(() {
+                  dropdownvalue = newV!;
+                });
+              },
+              items: <String>[
+                'Newest First',
+                'Oldest First',
+                'Value High-Low',
+                'Value Low-High'
+              ].map((String value) {
+                return DropdownMenuItem(
+                  value: value,
+                  child: Text(
+                    value,
+                    style: TextStyle(fontSize: 14),
+                  ),
+                );
+              }).toList(),
+            )),
+        Container(  //Conatiner for transaction types
+
+        ),
       ],
-    );
-  }
-}
-
-
-/// This is the stateful widget that the main application instantiates.
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  String dropdownValue = 'One';
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      iconSize: 24,
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? newValue) {
-        setState(() {
-          dropdownValue = newValue!;
-        });
-      },
-      items: <String>['One', 'Two', 'Free', 'Four']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
     );
   }
 }
