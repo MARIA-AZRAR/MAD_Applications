40a1731ed23fab350fe73e4f4792634346cdcb4c
MariaAzrar <mariaazrar2000@gmail.com>
2021-05-04 07:54:18 +0500

    Button added

--- webapiintegration/lib/Widgets/AddRecords.dart
+++ webapiintegration/lib/Widgets/AddRecords.dart
@@ -13,15 +13,13 @@ class AddRecords extends StatefulWidget {
 }
 
 //declaring global controller
-  TextEditingController _firstNameController = new TextEditingController();
-  TextEditingController _lastNameController = new TextEditingController();
-  TextEditingController _genderController = new TextEditingController();
-  TextEditingController _emailController = new TextEditingController();
-  TextEditingController _phoneController = new TextEditingController();
+TextEditingController _firstNameController = new TextEditingController();
+TextEditingController _lastNameController = new TextEditingController();
+TextEditingController _genderController = new TextEditingController();
+TextEditingController _emailController = new TextEditingController();
+TextEditingController _phoneController = new TextEditingController();
 
-  late var navigatorKey;
-
-
+late var navigatorKey;
 
 class _AddRecordsState extends State<AddRecords> {
   @override
@@ -34,98 +32,94 @@ class _AddRecordsState extends State<AddRecords> {
         ),
         drawer: Sidebar(),
         body: Container(
-          padding: EdgeInsets.fromLTRB(100, 50, 100, 14),
-          child: new Column(
-            children: [
-          new ListTile(
-            leading: const Icon(Icons.person),
-            title: new TextField(
-              controller: _firstNameController,
-              decoration: new InputDecoration(
-                hintText: "First Name",
-              ),
-            ),
-          ),
-          new ListTile(
-            leading: const Icon(Icons.person),
-            title: new TextField(
-              controller: _lastNameController,
-              decoration: new InputDecoration(
-                hintText: "Last Name",
-              ),
-            ),
-          ),
-          new ListTile(
-            leading: const Icon(Icons.person_add_alt_rounded),
-            title: new TextField(
-              controller: _genderController,
-              decoration: new InputDecoration(
-                hintText: "Gender",
-              ),
-            ),
-          ),
-          new ListTile(
-            leading: const Icon(Icons.email),
-            title: new TextField(
-              controller: _emailController,
-              decoration: new InputDecoration(
-                hintText: "Email",
-              ),
-            ),
-          ),
-          new ListTile(
-            leading: const Icon(Icons.phone),
-            title: new TextField(
-              controller: _phoneController,
-              decoration: new InputDecoration(
-                hintText: "Phone",
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-            child:RaisedButton.icon(
-                padding: EdgeInsets.all(20),
-                onPressed: consumerRegisteration,
-                icon: Icon(
-                  Icons.upload,
-                   color: Colors.white,
+            padding: EdgeInsets.fromLTRB(100, 50, 100, 14),
+            child: new Column(children: [
+              new ListTile(
+                leading: const Icon(Icons.person),
+                title: new TextField(
+                  controller: _firstNameController,
+                  decoration: new InputDecoration(
+                    hintText: "First Name",
+                  ),
                 ),
-                textColor: Colors.white,
-                label: Text("Submit"),
-                color: Colors.teal,
-                elevation: 6,
               ),
-          ),
-        ]
-
-        )
-        )
-        );
+              new ListTile(
+                leading: const Icon(Icons.person),
+                title: new TextField(
+                  controller: _lastNameController,
+                  decoration: new InputDecoration(
+                    hintText: "Last Name",
+                  ),
+                ),
+              ),
+              new ListTile(
+                leading: const Icon(Icons.person_add_alt_rounded),
+                title: new TextField(
+                  controller: _genderController,
+                  decoration: new InputDecoration(
+                    hintText: "Gender",
+                  ),
+                ),
+              ),
+              new ListTile(
+                leading: const Icon(Icons.email),
+                title: new TextField(
+                  controller: _emailController,
+                  decoration: new InputDecoration(
+                    hintText: "Email",
+                  ),
+                ),
+              ),
+              new ListTile(
+                leading: const Icon(Icons.phone),
+                title: new TextField(
+                  controller: _phoneController,
+                  decoration: new InputDecoration(
+                    hintText: "Phone",
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                child: RaisedButton.icon(
+                  padding: EdgeInsets.all(20),
+                  onPressed: consumerRegisteration,
+                  icon: Icon(
+                    Icons.upload,
+                    color: Colors.white,
+                  ),
+                  textColor: Colors.white,
+                  label: Text("Submit"),
+                  color: Colors.teal,
+                  elevation: 6,
+                ),
+              ),
+            ])));
   }
 }
 
-
 Future consumerRegisteration() async {
+  String fistName = _firstNameController.text;
+  String lastName = _lastNameController.text;
+  String gender = _genderController.text;
+  String email = _emailController.text;
+  String phone = _phoneController.text;
 
-   String fistName = _firstNameController.text;
-   String lastName = _lastNameController.text;
-   String gender = _genderController.text;
-   String email = _emailController.text;
-   String phone = _phoneController.text;
+  var url = "https://pcc.edu.pk/ws/create/rs_consumers.php";
 
-   
+  var data = {
+    'firstName': fistName,
+    'lastName': lastName,
+    'gender': gender,
+    'email': email,
+    'phone': phone
+  };
 
-   var url = "https://pcc.edu.pk/ws/create/rs_consumers.php";
+  var response = await http.post(Uri.parse(url), body: json.encode(data));
 
-   var data = {'firstName':fistName,'lastName':lastName,'gender':gender,'email':email,'phone':phone};
-   print(data);
-   var response = await http.post(Uri.parse(url), body: json.encode(data));
+  var message = jsonDecode(response.body);
 
-   var message = jsonDecode(response.body);
-
-   if(response.statusCode == 200){
-     print(message);
+  if (response.statusCode == 200) {
     showDialog(
       context: navigatorKey,
       builder: (context) {
@@ -133,13 +127,20 @@ Future consumerRegisteration() async {
           title: Text("Response Message"),
           content: Text(message['message']),
           elevation: 8,
-          contentPadding: EdgeInsets.fromLTRB(20,40,20,100),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(23)),
+          contentPadding: EdgeInsets.fromLTRB(20, 40, 20, 50),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(23)),
+          actions: [
+            FlatButton(
+              padding: EdgeInsets.fromLTRB(0, 0, 10, 16),
+              child: Text("OK", style: TextStyle(color: Colors.blue),),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
         );
       },
     );
-
-   }
   }
-
-
+}
